3c8d6cd1f9de21dbf03f4bc770cdba36ad78879d
itrujnara <itrujnara@wp.pl>
2024-11-18 20:42:55 +0100

    Improve down arrow position on small screens

--- src/components/sections/welcome_section.tsx
+++ src/components/sections/welcome_section.tsx
@@ -6,8 +6,8 @@ export default function WelcomeSection() {
     <div className="w-full h-[calc(100svh-100px)] md:h-[calc(100svh-50px)]">
       <h1 id="top" className="hidden"></h1>
       <TextSwitcher />
-      <div className="absolute ml-[50%] mr-auto -translate-x-[50%] bottom-2">
-        <ArrowDown />
+      <div className="absolute ml-[50%] mr-auto -translate-x-[50%] bottom-16 md:bottom-2">
+        <ArrowDown className="w-10 h-10 md:w-6 md:h-6" />
       </div>
     </div>
   )
