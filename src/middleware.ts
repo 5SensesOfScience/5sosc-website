17e7daee9730fbff4ece9ab4e22cc3abc315e73b
JosepCru <josepcrugixtv@gmail.com>
2024-11-19 10:43:35 +0100

    Errors solved

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,5 @@ export default createMiddleware(routing)
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ["/", "/(ca|en|pl)/:path*"],
+  matcher: ["/", "/(ca|en|es|pl)/:path*"],
 }
