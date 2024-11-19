e1c48a5d0a310f824c01be656d8ed65cfba09cd8
itrujnara <itrujnara@wp.pl>
2024-11-19 18:20:09 +0100

    Fix header link bug

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -9,53 +9,61 @@ import {
 import Image from "next/image"
 import { ModeToggle } from "@/components/ui/mode-toggle"
 import Hamburger from "@/components/ui/hamburger"
-import { useTranslations } from "next-intl"
+import { useLocale, useTranslations } from "next-intl"
 import LanguageSwitcher from "../ui/language-switcher"
 
-function Nav({ className }: { className?: string }) {
+function Nav({ className, locale }: { className?: string; locale: string }) {
   const t = useTranslations("Header")
+
+  function makeLink(path: string) {
+    return locale + "/" + path
+  }
 
   return (
     <nav className={className}>
       <NavigationMenu>
         <NavigationMenuList className="flex-col md:flex-row">
           <NavigationMenuItem>
-            <Link href="/#top" passHref className="scroll-smooth">
+            <Link href={makeLink("#top")} passHref className="scroll-smooth">
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                 {t("home")}
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="/#venue" passHref className="scroll-smooth">
+            <Link href={makeLink("#venue")} passHref className="scroll-smooth">
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                 {t("venue")}
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="/#program" passHref className="scroll-smooth">
+            <Link
+              href={makeLink("#program")}
+              passHref
+              className="scroll-smooth"
+            >
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                 {t("program")}
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="/#senses" passHref className="scroll-smooth">
+            <Link href={makeLink("#senses")} passHref className="scroll-smooth">
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                 {t("senses")}
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="/#team" passHref className="scroll-smooth">
+            <Link href={makeLink("#team")} passHref className="scroll-smooth">
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                 {t("team")}
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="/#findus" passHref className="scroll-smooth">
+            <Link href={makeLink("#findus")} passHref className="scroll-smooth">
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                 {t("find_us")}
               </NavigationMenuLink>
@@ -75,15 +83,22 @@ function Nav({ className }: { className?: string }) {
   )
 }
 
-function HamburgerNav({ className }: { className?: string }) {
+function HamburgerNav({
+  className,
+  locale,
+}: {
+  className?: string
+  locale: string
+}) {
   return (
     <Hamburger className={className}>
-      <Nav />
+      <Nav locale={locale} />
     </Hamburger>
   )
 }
 
 export default function Header() {
+  const locale = useLocale()
   return (
     <div className="sticky top-0 z-10 w-full h-content p-4 flex flex-col md:flex-row items-center justify-center md:justify-between bg-background">
       <Link href="/" passHref className="h-[50px] md:min-h-0">
@@ -95,8 +110,8 @@ export default function Header() {
           />
         </div>
       </Link>
-      <Nav className="hidden md:flex" />
-      <HamburgerNav className="md:hidden" />
+      <Nav locale={locale} className="hidden md:flex" />
+      <HamburgerNav locale={locale} className="md:hidden" />
     </div>
   )
 }
