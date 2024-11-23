0c5e7056f1cc475769350440b937979bd6d2a8a3
itrujnara <itrujnara@wp.pl>
2024-11-23 20:20:14 +0100

    Add translations to 404

--- src/app/[locale]/(main)/not-found.tsx
+++ src/app/[locale]/(main)/not-found.tsx
@@ -1,16 +1,18 @@
 "use client"
 
 import { Link } from "@/i18n/routing"
+import { useTranslations } from "next-intl"
 
 export default function NotFound() {
+  const t = useTranslations("NotFound")
   return (
     <div className="px-4">
       <h1 className="text-3xl font-fancy text-center md:text-left">
-        Not Found
+        {t("heading")}
       </h1>
       <hr className="my-2" />
-      <p className="text-lg">This page does not exist.</p>
-      <Link href="/"></Link>
+      <p className="text-lg">{t("message")}</p>
+      <Link href="/">{t("prompt")}</Link>
     </div>
   )
 }
