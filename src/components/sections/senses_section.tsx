59f87ecac7191f29ca9dc5f55da297aafda1f2f3
itrujnara <itrujnara@wp.pl>
2025-03-11 20:16:23 +0100

    Automate sense section

--- src/components/sections/senses_section.tsx
+++ src/components/sections/senses_section.tsx
@@ -1,121 +1,73 @@
-import { useTranslations } from "next-intl"
 import SeeMoreButton from "../ui/see-more"
-import generateGrid from "../ui/sense-grid";
+import { getPostsByCategory } from "@/lib/server_utils"
+import { getTranslations } from "next-intl/server"
+import { Post } from "@/lib/server_utils"
+import Highlight from "../ui/highlight"
 
-const senses = [
-  {
-    name: "see",
-    sense: "sight",
-    imagesLeft: [{ src: "/example.jpg", href: "/sight" }, { src: "/example.jpg", href: "/sight" }],
-    imagesRight: [
-      { src: "/example.jpg", href: "/sight" },
-      { src: "/example.jpg", href: "/sight" },
-      { src: "/example.jpg", href: "/sight" },
-      { src: "/example.jpg", href: "/sight" },
-      { src: "/example.jpg", href: "/sight" },
-      { src: "/example.jpg", href: "/sight" },
-    ]
-  },
-  {
-    name: "hear",
-    sense: "hearing",
-    imagesLeft: [{ src: "/example.jpg", href: "/hearing" }, { src: "/example.jpg", href: "/hearing" }],
-    imagesRight: [
-      { src: "/example.jpg", href: "/hearing" },
-      { src: "/example.jpg", href: "/hearing" },
-      { src: "/example.jpg", href: "/hearing" },
-      { src: "/example.jpg", href: "/hearing" },
-      { src: "/example.jpg", href: "/hearing" },
-      { src: "/example.jpg", href: "/hearing" },
-    ]
-  },
-  {
-    name: "smell",
-    sense: "smell",
-    imagesLeft: [{ src: "/example.jpg", href: "/smell" }, { src: "/example.jpg", href: "/smell" }],
-    imagesRight: [
-      { src: "/example.jpg", href: "/smell" },
-      { src: "/example.jpg", href: "/smell" },
-      { src: "/example.jpg", href: "/smell" },
-      { src: "/example.jpg", href: "/smell" },
-      { src: "/example.jpg", href: "/smell" },
-      { src: "/example.jpg", href: "/smell" },
-    ]
-  },
-  {
-    name: "taste",
-    sense: "taste",
-    imagesLeft: [{ src: "/example.jpg", href: "/taste" }, { src: "/example.jpg", href: "/taste" }],
-    imagesRight: [
-      { src: "/example.jpg", href: "/taste" },
-      { src: "/example.jpg", href: "/taste" },
-      { src: "/example.jpg", href: "/taste" },
-      { src: "/example.jpg", href: "/taste" },
-      { src: "/example.jpg", href: "/taste" },
-      { src: "/example.jpg", href: "/taste" },
-    ]
-  },
-  {
-    name: "touch",
-    sense: "touch",
-    imagesLeft: [{ src: "/example.jpg", href: "/touch" }, { src: "/example.jpg", href: "/touch" }],
-    imagesRight: [
-      { src: "/example.jpg", href: "/touch" },
-      { src: "/example.jpg", href: "/touch" },
-      { src: "/example.jpg", href: "/touch" },
-      { src: "/example.jpg", href: "/touch" },
-      { src: "/example.jpg", href: "/touch" },
-      { src: "/example.jpg", href: "/touch" },
-    ]
-  },
-];
+export default async function SensesSection({ locale }: { locale: string }) {
+  const t = await getTranslations("TextSwitcher")
 
-export default function SensesSection() {
-  const t = useTranslations("TextSwitcher");
-  return (
-    <div id = "senses" className="flex flex-col gap-8">
-      {senses.map((sense, index) => (
-        <div key={index} className="flex flex-col gap-6">
-          {/* Title */}
-          <h2 className="text-2xl font-fancy text-center md:text-left">
-            <span className={`${"text-"+sense.sense}`}>{t(sense.name)}</span>{" "}
-            <span>{t("science")}</span>
-          </h2>
-
-          {/* Grid of Images */}
-          {generateGrid({ imagesLeft: sense.imagesLeft, imagesRight: sense.imagesRight })}
-
-          {/* See More Button */}
-          <SeeMoreButton href={"/" + sense.sense} label={t("seemore")} />
-        </div>
-      ))}
-    </div>
-  );
-}
-
-
-
-
-
-
-
-
-
-/* 
-        </Link>
-        <Link className="text-xl" href="/hearing">
-          Hearing
-        </Link>
-        <Link className="text-xl" href="/smell">
-          Smell
-        </Link>
-        <Link className="text-xl" href="/taste">
-          Taste
-        </Link>
-        <Link className="text-xl" href="/touch">
-          Touch
-        </Link>
-      </div>
-    </div>
+  const posts: Array<Post> = await Promise.all(
+    await getPostsByCategory(locale, "posts")
   )
-}*/
+
+  const senses = [
+    {
+      name: "see",
+      sense: "sight",
+      posts: posts.filter((post) => post.sense === "Sight"),
+    },
+    {
+      name: "hear",
+      sense: "hearing",
+      posts: posts.filter((post) => post.sense === "Hearing"),
+    },
+    {
+      name: "smell",
+      sense: "smell",
+      posts: posts.filter((post) => post.sense === "Smell"),
+    },
+    {
+      name: "taste",
+      sense: "taste",
+      posts: posts.filter((post) => post.sense === "Taste"),
+    },
+    {
+      name: "touch",
+      sense: "touch",
+      posts: posts.filter((post) => post.sense === "Touch"),
+    },
+  ]
+
+  return (
+    <>
+      <h2 className="text-4xl font-fancy mb-12 text-center">
+        {t("virtual_exhibition")}
+      </h2>
+      <div id="senses" className="flex flex-col gap-8 mb-6">
+        {senses.map(
+          (sense, index) =>
+            sense.posts.length > 0 && (
+              <div key={index} className="flex flex-col gap-6">
+                {/* Title */}
+                <h2 className="text-2xl font-fancy text-center">
+                  <span className={`${"text-" + sense.sense}`}>
+                    {t(sense.name)}
+                  </span>{" "}
+                  <span>{t("science")}</span>
+                </h2>
+
+                <div className="w-full flex flex-col md:flex-row gap-10 items-center justify-center">
+                  {sense.posts.map((post, index) => (
+                    <div key={index} className="w-full max-w-[300px]">
+                      <Highlight key={index} post={post} />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )
+        )}
+      </div>
+    </>
+  )
+}
